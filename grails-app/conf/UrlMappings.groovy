fd5258a1f2683b6fc6fe49bdf1276c6ac4841624
jhillierdavis <josephhillier-davis@JHDMBP11.local>
2015-02-22 12:23:37 +0000

    Changed default home page
    Changed default home page from Grails default to Project listing.

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -7,7 +7,8 @@ class UrlMappings {
             }
         }
 
-        "/"(view:"/index")
+        // "/"(view:"/index")
+		"/"(controller: 'project', action: 'index')
         "500"(view:'/error')
 	}
 }
