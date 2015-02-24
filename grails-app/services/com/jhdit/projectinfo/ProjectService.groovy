652b206e16c5ace39d17e6515e93696cb81442c3
jhillierdavis <josephhillier-davis@JHDMBP11.local>
2015-02-24 00:19:50 +0000

    Refactored
    Used extract method to try to make more expressive

--- grails-app/services/com/jhdit/projectinfo/ProjectService.groovy
+++ grails-app/services/com/jhdit/projectinfo/ProjectService.groovy
@@ -4,312 +4,158 @@ import org.apache.commons.logging.LogFactory;
 
 import grails.transaction.Transactional
 
+/**
+ * Service to handle CRUD operations for Project entities
+ * Provides logic to automatically re-organise other project priorities where necessary
+ * 
+ * TODO: JHD: Concurrent access issues? (Synchronize?)  
+ */
+
 @Transactional
 class ProjectService {
 	private static final log = LogFactory.getLog(this)
+	
+	private static final FIELD_PROJECT_PRIORITY = 'priority'
 
 	def saveProject(Project project) {
 		assert project
 		
 		project.save(failOnError: true, flush:true)
-		reorganisePriorities(project)		
+		this.shiftPriorities(project.priority, project.code, false)
 	}
 	
-	def updateProject(Project project) {
+	def updateProject(final Project project) {
 		assert project
 		
-		def currentPriority = project.priority
-		def persistedPriority = project.getPersistentValue('priority')
-		if (persistedPriority == currentPriority)	{
-			persistedPriority = getPersistedPriority(project)
+		// Handle an update with an amended priority
+		if (project.isDirty(FIELD_PROJECT_PRIORITY))	{
+			this.updateProjectWithChangedPriority(project)
+			return	
 		}
-		if (persistedPriority == currentPriority)	{
-			persistedPriority = findGap(currentPriority)
-		}
+		
+		project.validate()
+		project.save(failOnError: true, flush:true)
+	}	
+	
+	def deleteProject(final Project project) {
+		assert project
+		
+		def deletePriority = project.priority
+		def deleteCode = project.code
+		
+		project.delete(failOnError: true, flush:true)
+		shiftPriorities(deletePriority, deleteCode, true)
+	}
+	
+	private def updateProjectWithChangedPriority(final Project project) {
+		def amendedPriority = project.priority
+		def previousPriority = findPreviousPriority(project, amendedPriority)
+		log.info "Project update: code: ${project.code} amendedPriority: ${amendedPriority} previousPriority: ${previousPriority}"
 
 		project.validate()
 		project.save(failOnError: true, flush:true)
 		
-		if (persistedPriority == currentPriority)	{
+		if (previousPriority == amendedPriority)	{
+			log.error "Project update with dirty priority (code: ${project.code}). However, amendedPriority: ${amendedPriority} equals previousPriority: ${previousPriority}"
 			return
 		}
 		
-		boolean isIncreasedPriority =  persistedPriority > project.priority
-		log.info "Update ${project.code} updatedPriority: ${project.priority} persistedPriority: ${persistedPriority}"
-
-		def matches = Project.list()
-		
-		if (matches && matches.size() > 1)	{
-			// Re-organise projects which have the same or greater priorities
-			int x = 0
-			for (Project existingProject: matches)	{
-				x++
-				if (existingProject == project) {
-					continue
-				}
+		final boolean isIncreasedPriority =  previousPriority > project.priority
+		this.shiftOtherProjectPriorities(project, isIncreasedPriority)
+	}
 	
-				if (existingProject.priority != x)	{
-					existingProject.priority = x
-					existingProject.save()
-				} else if (existingProject.priority == project.priority)	{
-					if (isIncreasedPriority)	{
-						existingProject.priority++
-					} else {
-						existingProject.priority--
-					}
-					existingProject.save()
+	/**
+	 * Re-organise all other project priorities around the supplied project 
+	 */
+	
+	private def shiftOtherProjectPriorities(final Project project, final boolean isIncrementOthers)	{
+		def matches = Project.list()
+		if (!matches || matches.size() <= 1) {
+			return  // Nothing to do
+		}
+
+		// Re-organise projects which have the same or greater priorities
+		int index = 0
+		for (Project existingProject: matches)	{
+			index++
+			if (existingProject == project) {
+				continue
+			}
+
+			if (existingProject.priority != index)	{
+				existingProject.priority = index
+				existingProject.save()
+			} else if (existingProject.priority == project.priority)	{
+				if (isIncrementOthers)	{
+					existingProject.priority++
+				} else {
+					existingProject.priority--
 				}
-				
+				existingProject.save()
 			}
 		}
 	}
+
+	/**
+	 * Obtain the previous project priority, when dirty, via various methods (since some are seemingly unreliable)
+	 * @param project Target project
+	 * @param currentPriority 
+	 * @return Previous priority
+	 */
 	
-	def findGap(def defaultValue)	{
+	private def findPreviousPriority(final Project project, def currentPriority)	{
+		def previousPriority = project.getPersistentValue(FIELD_PROJECT_PRIORITY)
+		if (previousPriority == currentPriority)	{
+			log.info "Attempting to get previous priority via persisted entity for project code: ${project.code}"
+			previousPriority = getPersistedPriority(project)
+		}
+		if (previousPriority == currentPriority)	{
+			log.info "Attempting to get previous priority via gap for project code: ${project.code}"
+			previousPriority = findGap(currentPriority)
+		}
+		return previousPriority
+	}
+	
+	private def getPersistedPriority(final Project project)	{
+		Project persistedProject = Project.findByCode(project.code)
+		return persistedProject.priority
+	}
+	
+	private def findGap(def defaultValue)	{
 		for (i in 1..Project.count())	{
 			if (!Project.findByPriority(i))	{
 				return i
 			}
 		}
 		return defaultValue
-	}
+	}	
 	
-	/*
-	def updateProject(Project project) {
-		assert project
-		
-		project.validate()
-		if (project.hasErrors())	{
-			return
-		}
-		
-		def persistedPriority = project.getPersistentValue('priority')
-		if (persistedPriority == project.priority)	{
-			persistedPriority = getPersistedPriority(project)
-		}	
-		
-		if (persistedPriority == project.priority)	{
-			throw new SystemException("Cannot determine change in priority: Update ${project.code} updatedPriority: ${project.priority} persistedPriority: ${persistedPriority}")
-		}
-		
-		boolean isIncreasedPriority =  persistedPriority > project.priority
-		log.info "Update ${project.code} updatedPriority: ${project.priority} persistedPriority: ${persistedPriority}"
-
-		
-		
-		def matches = Project.list()
-		
-		project.save()
-
-		if (matches && matches.size() > 1)	{
-			// Re-organise projects which have the same or greater priorities
-			int x = 0
-			for (Project existingProject: matches)	{
-				x++
-				if (existingProject == project) {
-					continue
-				}
+	/**
+	 * Shift any matching priorities (from a target) i.e. towards 1 (the highest priority)
+	 * @param targetPriority  Starting priority to match
+	 * @param targetProjectCode Code of project to be excluded
+	 * @param isDecrement Shift towards 1 (the highest priority) if true, otherwise away
+	 */
 	
-				if (existingProject.priority != x)	{
-					existingProject.priority = x
-					existingProject.save()
-				} else if (existingProject.priority == project.priority)	{
-					if (isIncreasedPriority)	{
-						existingProject.priority++
-					} else {
-						existingProject.priority--
-					}
-					existingProject.save()
-				}
-				
-			}
-		}
-	}
-	*/
+	private void shiftPriorities(def targetPriority, def targetProjectCode, final boolean isDecrement)	{
+		def matches = Project.findAllByPriorityGreaterThanEquals(targetPriority, [sort: FIELD_PROJECT_PRIORITY, order: "desc"])
 
-/*
-	def updateProject(Project project) {
-		assert project
-		
-		def updatedPriority = project.priority
- 		def persistedPriority = project.getPersistentValue('priority')
-		// def persistedPriority = getPersistedPriority(project)
-		System.out.println "Update ${project.code} ${project.priority} persistedPriority: ${persistedPriority}"
-
-		System.out.println "Changed priority: updated:${updatedPriority} persisted: ${persistedPriority} ${project.code}"
-		// assert project.isDirty('priority') // Ensure dealing with an update
-		// assert project.isDirty()
-		
-		if (updatedPriority == persistedPriority)	{
-			System.out.println "Doesn't appear to be an update, priorities match!"
-			project.save(failOnError: true, flush: true)
-			return
-		}
-		
-		if (persistedPriority < updatedPriority)	{	
-			// Priority decreased	
-			def matches = Project.findAllByPriorityLessThanEquals(updatedPriority, [sort: "priority", order: "asc"])
-	
-			if (!matches)	{
-				return
-			}
-			
-			// Re-organise projects which have the same or greater priorities
-			for (Project existingProject: matches)	{
-				if (existingProject == project) {
-					continue
-				}
-				
-				if (existingProject.priority > persistedPriority)	{
-					existingProject.priority--
-					existingProject.save()
-				}				
-			}
-		} else	{
-			// 	Priority increased
-			def matches = Project.findAllByPriorityGreaterThanEquals(updatedPriority, [sort: "priority", order: "desc"])
-			if (!matches)	{
-				return
-			}
-			
-			// Re-organise projects which have the same or greater priorities
-			for (Project existingProject: matches)	{
-				if (existingProject == project) {
-					continue
-				}
-				
-				if (existingProject.priority <= persistedPriority)	{
-					existingProject.priority++
-					existingProject.save()
-				}				
-			}
-
-		}
-		
-		// Update to intended value
-		project.priority = updatedPriority
-		project.save(flush: true)	
-	}
-*/	
-	private def getPersistedPriority(final Project project)	{
-		// NB: project.getPersistentValue('priority') doesn't appear to work unfortunately in integration tests!
-		
-		for (i in 0 ..Project.count())	{
-			Project existing = Project.findByPriority(i)
-			if (existing && existing.code == project.code)	{
-				return i
-			}
-		}
-		return project.priority
-	}
-	
-	
-	
-/*
-	private void updatePriorities(Project project)	{
-		assert project // Ensure not NULL
-
-		
-		
-		def tempPriority = 0
-		def updatedPriority = project.priority
-		def persistedPriority = project.getPersistentValue('priority')
-		System.out.println "Changed priority: updated:${updatedPriority} peristed: ${persistedPriority} ${project}"
-		// assert project.isDirty('priority') // Ensure dealing with an update
-		assert project.isDirty()
-		
-		project.priority = tempPriority
-		project.save(flush: true)
-		
-
-		if (persistedPriority < updatedPriority)	{	
-			// Priority decreased	
-			def matches = Project.findAllByPriorityLessThanEquals(updatedPriority, [sort: "priority", order: "asc"])
-	
-			if (!matches)	{
-				return
-			}
-			
-			// Re-organise projects which have the same or greater priorities
-			for (Project existingProject: matches)	{
-				if (existingProject == project) {
-					continue
-				}
-				
-				if (existingProject.priority > persistedPriority)	{
-					existingProject.priority--
-					existingProject.save()
-				}				
-			}
-		} else	{
-			// 	Priority increased
-			def matches = Project.findAllByPriorityGreaterThanEquals(updatedPriority, [sort: "priority", order: "desc"])
-			if (!matches)	{
-				return
-			}
-			
-			// Re-organise projects which have the same or greater priorities
-			for (Project existingProject: matches)	{
-				if (existingProject == project) {
-					continue
-				}
-				
-				if (existingProject.priority <= persistedPriority)	{
-					existingProject.priority++
-					existingProject.save()
-				}				
-			}
-
-		}
-		
-		project.priority = updatedPriority
-		project.save(flush:true)
-		
-	}
-*/
-	def deleteProject(final Project project) {
-		assert project
-		
-		def matches = Project.findAllByPriorityGreaterThanEquals(project.priority, [sort: "priority", order: "desc"])
-		
-		project.delete(failOnError: true, flush:true)
-		
 		if (!matches)	{
-			return
+			return // Nothing to do!
 		}
 
 		// Adjust project priorities
 		for (Project existingProject: matches)	{
-			if (existingProject == project) {
-				continue
+			if (existingProject.code == targetProjectCode) {
+				continue // Ignore target
 			}
-			existingProject.priority--
-			existingProject.save(failOnError: true, flush:true)	
+			if (isDecrement)	{
+				existingProject.priority--
+			} else	{
+				existingProject.priority++
+			}
+			log.info "Changed existingProject code: ${existingProject.code} priority to ${existingProject.priority}"
+			existingProject.save(failOnError: true, flush:true)
 		}
 	}
-	
-	
-	private def reorganisePriorities(Project project)	{
-		assert project
-		
-		def matches = Project.findAllByPriorityGreaterThanEquals(project.priority, [sort: "priority", order: "desc"])
-
-		if (!matches)	{
-			return
-		}
-		
-		// Re-organise projects which have the same or greater priorities
-		for (Project existingProject: matches)	{
-			
-
-			if (existingProject == project) {
-//				System.out.println "currentProject: ${project}"
-//				System.out.println "existingProject: ${existingProject}"
-				continue
-			}
-			
-			existingProject.priority++
-			log.info "Incrementing existingProject: ${existingProject.code} priority to ${existingProject.priority}"
-			existingProject.save(failOnError: true, flush:true)	
-		}
-	}	
 }
