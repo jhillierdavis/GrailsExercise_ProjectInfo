3f93ee1e9e10b9d530b4048653a811587ad49024
jhillierdavis <josephhillier-davis@JHDMBP11.local>
2015-02-24 10:05:05 +0000

    Re-formatted

--- grails-app/controllers/com/jhdit/projectinfo/ProjectController.groovy
+++ grails-app/controllers/com/jhdit/projectinfo/ProjectController.groovy
@@ -18,100 +18,112 @@ import grails.transaction.Transactional
 class ProjectController {
 	def projectService // Injected
 
-    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+	static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
 
-    def index(Integer max) {
-        params.max = Math.min(max ?: 10, 100)
-        respond Project.list(params), model:[projectInstanceCount: Project.count()]
-    }
+	def index(Integer max) {
+		params.max = Math.min(max ?: 10, 100)
+		respond Project.list(params), model:[projectInstanceCount: Project.count()]
+	}
 
-    def show(Project projectInstance) {
-        respond projectInstance
-    }
+	def show(Project projectInstance) {
+		respond projectInstance
+	}
 
-    def create() {
-        respond new Project(params)
-    }
+	def create() {
+		respond new Project(params)
+	}
 
-    @Transactional
-    def save(Project projectInstance) {
-        if (projectInstance == null) {
-            notFound()
-            return
-        }
-		
-		if (projectInstance.hasErrors()) {
-				respond projectInstance.errors, view:'create'
-				return
+	@Transactional
+	def save(Project projectInstance) {
+		if (projectInstance == null) {
+			notFound()
+			return
 		}
-		
+
+		if (projectInstance.hasErrors()) {
+			respond projectInstance.errors, view:'create'
+			return
+		}
+
 		assert this.projectService
-		this.projectService.saveProject projectInstance		
+		this.projectService.saveProject projectInstance
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.created.message', args: [message(code: 'project.label', default: 'Project'), projectInstance.id])
-                redirect projectInstance
-            }
-            '*' { respond projectInstance, [status: CREATED] }
-        }
-    }
+		request.withFormat {
+			form multipartForm {
+				flash.message = message(code: 'default.created.message', args: [
+					message(code: 'project.label', default: 'Project'),
+					projectInstance.id
+				])
+				redirect projectInstance
+			}
+			'*' { respond projectInstance, [status: CREATED] }
+		}
+	}
 
-    def edit(Project projectInstance) {
-        respond projectInstance
-    }
+	def edit(Project projectInstance) {
+		respond projectInstance
+	}
 
-    @Transactional
-    def update(Project projectInstance) {
-        if (projectInstance == null) {
-            notFound()
-            return
-        }
+	@Transactional
+	def update(Project projectInstance) {
+		if (projectInstance == null) {
+			notFound()
+			return
+		}
 
 		if (projectInstance.hasErrors()) {
 			respond projectInstance.errors, view:'edit'
 			return
 		}
-		
-		assert this.projectService		
+
+		assert this.projectService
 		this.projectService.updateProject projectInstance
-		
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.updated.message', args: [message(code: 'Project.label', default: 'Project'), projectInstance.id])
-                redirect projectInstance
-            }
-            '*'{ respond projectInstance, [status: OK] }
-        }
-    }
 
-    @Transactional
-    def delete(Project projectInstance) {
+		request.withFormat {
+			form multipartForm {
+				flash.message = message(code: 'default.updated.message', args: [
+					message(code: 'Project.label', default: 'Project'),
+					projectInstance.id
+				])
+				redirect projectInstance
+			}
+			'*'{ respond projectInstance, [status: OK] }
+		}
+	}
 
-        if (projectInstance == null) {
-            notFound()
-            return
-        }
+	@Transactional
+	def delete(Project projectInstance) {
+
+		if (projectInstance == null) {
+			notFound()
+			return
+		}
 
 		assert this.projectService
 		this.projectService.deleteProject projectInstance
 
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Project.label', default: 'Project'), projectInstance.id])
-                redirect action:"index", method:"GET"
-            }
-            '*'{ render status: NO_CONTENT }
-        }
-    }
+		request.withFormat {
+			form multipartForm {
+				flash.message = message(code: 'default.deleted.message', args: [
+					message(code: 'Project.label', default: 'Project'),
+					projectInstance.id
+				])
+				redirect action:"index", method:"GET"
+			}
+			'*'{ render status: NO_CONTENT }
+		}
+	}
 
-    protected void notFound() {
-        request.withFormat {
-            form multipartForm {
-                flash.message = message(code: 'default.not.found.message', args: [message(code: 'project.label', default: 'Project'), params.id])
-                redirect action: "index", method: "GET"
-            }
-            '*'{ render status: NOT_FOUND }
-        }
-    }
+	protected void notFound() {
+		request.withFormat {
+			form multipartForm {
+				flash.message = message(code: 'default.not.found.message', args: [
+					message(code: 'project.label', default: 'Project'),
+					params.id
+				])
+				redirect action: "index", method: "GET"
+			}
+			'*'{ render status: NOT_FOUND }
+		}
+	}
 }
